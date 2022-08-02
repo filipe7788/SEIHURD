f7f2e539594ce0d683d71863ca704319e1a6726d
Filipe Cruz <filipe.cruz.ribeiro@gmail.com>
2022-08-02 11:57:38 -0300

    melhorando organizacao dos arquivos e definindo ponto de partida

--- versões antigas/seiihurd_psi4 (1).m
+++ versões antigas/seiihurd_psi4 (1).m
@@ -4,9 +4,8 @@ clc
 
 
 %% ============ Load Data ============
-load 'input_data/covidbahia_test_Vac'
+load 'input_data/yourmatfile.mat'
 load 'input_data/Psi_Ba13set'
-
 npd = length(Psi);
 Nfo = 8;            % Filter order
 for k = 1:npd
@@ -17,44 +16,54 @@ for k = 1:npd
     end
 end
 
-coviddata=covidbahia;
+coviddata=c;
 
-Cases_dado      = coviddata(:,1)';
-R_dado          = coviddata(:,7)';
-D_dado          = coviddata(:,5)';
-H_dado          = coviddata(:,2)';
-U_dado          = coviddata(:,3)';
-V_dado          = coviddata(:,8)';
-data_in = datetime(2020,03,06);
+Cases_dado      = coviddata(1:193,1)';
+Cases_dado      = cell2mat(Cases_dado);
+R_dado          = coviddata(1:193,7)';
+R_dado          = cell2mat(R_dado);
+D_dado          = coviddata(1:193,5)';
+D_dado          = cell2mat(D_dado);
+H_dado          = coviddata(1:193,2)';
+H_dado          = cell2mat(H_dado);
+U_dado          = coviddata(1:193,3)';
+U_dado          = cell2mat(U_dado);
+V_dado          = coviddata(1:193,8)';
+V_dado          = cell2mat(V_dado);
+data_in = datetime(2021,01,01);
 Nw_cases        = diff(Cases_dado);
 Nw_deaths       = diff(D_dado);
+Nw_vac          = diff(V_dado);
+Nw_vac          = [Nw_vac,Nw_vac(192)];
 
-t_vac           = 100; %% day started the vaccination
+t_vac           = 19; %% day started the vaccination
 %% ============ Initial Condition ============
 
-D0          = 0;
 N0          = 14930634;
-R0          = 0;
-H0          = 0;
-U0          = 0;
-V0          = 1000/N0;  %% new compartment
+D0          = D_dado(1);
+R0          = 451240;
+H0          = H_dado(1);
+HV0         = 0;
+U0          = U_dado(1);
+UV0         = 0;
+V0          = V_dado(1);  %% new compartment
 Rv0         = 0; %% new compartment
-Is0         = 2.015439771376298e-06;
-Ia0         = 1.8028646508967777e-06;
-Iav0        = 1.8028646508967777e-06; %% new compartment
-Isv0        = 1.8028646508967777e-06; %% new compartment
-E0          = 1.7639153732952095e-06;
-Ev0         = 1.7639153732952095e-06; %% new compartment
-S0          = (1-Is0-Ia0-E0);
+Is0         = (Cases_dado(1)-451240);
+Ia0         = Is0*0.9;
+Iav0        = 0; %% new compartment
+Isv0        = 0; %% new compartment
+E0          = (518955-Cases_dado(1));
+Ev0         = 0; %% new compartment
+S0          = (N0-Is0-Ia0-E0);
 Nw0         = 0;
 NwV0        = 0;
-Model_0     = [S0,E0,V0,Ev0,Ia0,Is0,Iav0,Isv0,H0,U0,R0,Rv0,D0,Nw0,NwV0];
+Model_0     = [S0,E0,V0,Ev0,Ia0,Is0,Iav0,Isv0,H0,HV0,U0,UV0,R0,Rv0,D0,Nw0,NwV0]/N0;
 
 %% ============ Model ============
     
 tsim = length(U_dado)-1; 
     
-[t,y] = ode45(@(t,y)covid_odes(t,y,Psif),0:1:tsim,Model_0');
+[t,y] = ode45(@(t,y)covid_odes(t,y,Psif,Nw_vac),0:1:tsim,Model_0');
 
 Sm=N0.*y(:,1);
 Em=N0.*y(:,2);
@@ -65,12 +74,14 @@ Ism=N0.*y(:,6);
 Iavm=N0.*y(:,7);
 Isvm=N0.*y(:,8);
 Hm=N0.*y(:,9);
-Um=N0.*y(:,10);
-Rm=N0.*y(:,11);
-Rvm=N0.*y(:,12);
-Dm=N0.*y(:,13);
-Nwm=N0.*y(:,14);
-NwVm=N0.*y(:,15);
+Hv=N0.*y(:,10);
+Um=N0.*y(:,11);
+Uv=N0.*y(:,12);
+Rm=N0.*y(:,13);
+Rvm=N0.*y(:,14);
+Dm=N0.*y(:,15);
+Nwm=N0.*y(:,16);
+NwVm=N0.*y(:,17);
 
 %% ============ DATA ============
 
@@ -170,7 +181,7 @@ p3=plot(t_p(2:end),Nw_i,'b','LineWidth',3);
 p4=plot(t_dado(2:end),Nw_cases,'k.','MarkerSize',10);
 set(gca,'FontSize',20,'LineWidth',2,'FontWeight','Bold')
 xlim([t_dado(2) t_dado(tsim)])
-ylabel('New cases')
+%ylabel('New cases')
 
 figure(1)
 subplot(2,2,2)
@@ -229,24 +240,28 @@ title('SEIIHURD+\Psi and R^2')
 % save('output_data/seiihurd_psi_data.mat')
 
 %% ============ Model simulation ============
-function dydt = covid_odes(t,y,Psif)
-N=1;
+function dydt = covid_odes(t,y,Psif,Nw_vac)
+N=14930634;
 psi=Psif(floor(t+1));
+tau= Nw_vac(floor(t+1));
 
 k = 1/4;
 gamma_a = 1/3.5;
 gamma_s = 1/4;
-gamma_h = 0.1776863042741119;
+gamma_h = 0.18;
 gamma_u = 0.13342706158133355;
 mi_u = 0.4;
 qsi = 0.53;
-h = 0.06287612499693644;
+h = 0.06;
+h_v=0; % h_v=0.012;
 mi_h = 0.15;
 ome_h = 0.14;
-ome_u = 0.29;
-delta = 0.30906304338495505;
+ome_u = 0.4;
+% delta = 0.30906304338495505;
+delta = 0.31;
 p = 0.2;
 
+% gama_v, Iv, omega_v, miVU, omega_u
 
 if t<20.178
 beta=2.1317;    %beta=1.3987731952032998;
@@ -258,12 +273,28 @@ else
 beta=1;
 end
 
+beta_v = beta/2;
+
+% 
+% if t<20.178
+% beta=0.96;
+% beta_v=0;
+% elseif (t>=28.178-8)&&(t< 72.94)
+% beta=0.96; 
+% beta_v=0;
+% elseif (t>=72.94)&&(t< 148)
+% beta=0.96; 
+% beta_v=0;
+% else 
+% beta=0.96;
+% beta_v=0;
+% end
+
 %  New parameters
 %  Vaccination compartments parameters are 0 until starts the vaccination
 
 
-if t<100
-    tau=0;
+if t<19
     delta_av = 0;
     delta_sv = 0;
     phi_e = 0;
@@ -271,52 +302,65 @@ if t<100
     p_v = 0;
     gamma_av = 0;
     gamma_sv = 0;
+    gamma_vu=0;
     qsi_v = 0 ;
+    eps=0;
+    mi_vh=0;
+    mi_vu=0;
+    gamma_vh=0; %     gamma_vh=0.36;
 else
-    delta_av = 0.30906304338495505;
-    delta_sv = 0.30906304338495505;
-    phi_e = 0.8;
-    tau=(6.6976e-05)*1.1;
-    k_v=0.1;
+    delta_av = 0.31;
+    delta_sv = 0.31;
+    phi_e = 1/14;
+%     tau=(6.6976e-05)*1.1;
+    k_v=1/3;
     p_v = 0.1;
     gamma_av = 1/3.5;
     gamma_sv = 1/4;
-    qsi_v = 0.53 ;
+    gamma_vu=0.26;
+    qsi_v = 0.99;
+    eps=0.7;
+    mi_vh=0.03;
+    mi_vu=0.25;
+    gamma_vh=0.08;
 end
-
 
 S=y(1);
 E=y(2);
-V=y(3);
+Sv=y(3);
 Ev=y(4);
 Ia=y(5);
 Is=y(6);
 Iav=y(7);
 Isv=y(8);
 H=y(9);
-U=y(10);
-R=y(11);
-Rv=y(12);
-D=y(13);
-Nw=y(14);
-NwV=y(15);
+Hv=y(10);
+U=y(11);
+Uv=y(12);
+R=y(13);
+Rv=y(14);
+D=y(15);
+Nw=y(16);
+NwV=y(17);
 
-dSdt = -(1-psi)*beta*S*(Is+delta*Ia+delta_av*Iav+delta_sv*Isv)/N - tau*S;
+dSdt = -(1-psi)*beta*S*(Is+delta*Ia+delta_av*Iav+delta_sv*Isv)/N - tau/N;
 dEdt = (1-psi)*beta*S*(Is+delta*Ia+delta_av*Iav+delta_sv*Isv)/N - k*E;
-dVdt = tau*S - (1-psi)*beta*V*(Is+delta*Ia+delta_av*Iav+delta_sv*Isv)/N - phi_e*V;
-dEvdt = (1-psi)*beta*V*(Is+delta*Ia+delta_av*Iav+delta_sv*Isv)/N - k_v*Ev;
+dSvdt = tau/N - (1-psi)*beta_v*Sv*(Is+delta*Ia+delta_av*Iav+delta_sv*Isv)/N - phi_e*eps*Sv;
+dEvdt = (1-psi)*beta_v*Sv*(Is+delta*Ia+delta_av*Iav+delta_sv*Isv)/N - k_v*Ev;
 dIadt = (1-p)*k*E - gamma_a*Ia;
 dIsdt = p*k*E - gamma_s*Is;
 dIavdt = (1-p_v)*k_v*Ev - gamma_av*Iav;
 dIsvdt = p_v*k_v*Ev - gamma_sv*Isv;
-dHdt = qsi_v*gamma_sv*Isv + h*qsi*gamma_s*Is + (1-mi_u+ome_u*mi_u)*gamma_u*U - gamma_h*H;
+dHdt = h*qsi*gamma_s*Is + (1-mi_u+ome_u*mi_u)*gamma_u*U - gamma_h*H;
+dHvdt = h_v*qsi_v*gamma_sv*Isv + (1-mi_vu+ome_u*mi_vu)*gamma_vu*Uv - gamma_vu*Hv;
 dUdt = h*(1-qsi)*gamma_s*Is + ome_h*gamma_h*H - gamma_u*U;
-dRdt = gamma_a*Ia + (1-h)*gamma_s*Is + (1-mi_h)*(1-ome_h)*gamma_h*H;
-dRvdt = gamma_av*Iav + (1-qsi_v)*gamma_sv*Isv + phi_e*V;
-dDdt = (1-ome_h)*mi_h*gamma_h*H + (1-ome_u)*mi_u*gamma_u*U;
+dUvdt = h_v*(1-qsi_v)*gamma_sv*Isv + ome_h*gamma_h*Hv - gamma_u*Uv;
+dRdt = gamma_a*Ia + (1-h)*gamma_s*Is + (1-mi_h)*(1-ome_h)*gamma_h*H - phi_e*eps*Sv;
+dRvdt = gamma_av*Iav + (1-h)*gamma_sv*Isv + (1-mi_vh)*(1-ome_h)*(gamma_vh*Hv);
+dDdt = (1-ome_h)*(mi_h*gamma_h*H +mi_vh*gamma_vh*Hv) + (1-ome_h)*(mi_u*gamma_u*U+mi_vu*gamma_vu*Uv);
 dNwdt = p*k*E + p_v*k_v*Ev;
-dNwVdt = tau*S;
+dNwVdt = tau/N;
 
-dydt = [dSdt; dEdt; dVdt; dEvdt; dIadt; dIsdt; dIavdt; dIsvdt; dHdt; dUdt; dRdt; dRvdt; dDdt; dNwdt; dNwVdt];
+dydt = [dSdt; dEdt; dSvdt; dEvdt; dIadt; dIsdt; dIavdt; dIsvdt; dHdt; dHvdt; dUdt; dUvdt; dRdt; dRvdt; dDdt; dNwdt; dNwVdt];
 
 end
