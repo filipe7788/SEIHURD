3822d70490c7c97fdc20e9a5040607da8661cca8
filipe7788 <filipe.cruz.ribeiro@gmail.com>
2023-11-21 06:38:46 -0300

    fit longo feio

--- fit_mcmc/fits_tests/envio_resultado.py
+++ fit_mcmc/fits_tests/envio_resultado.py
@@ -1,31 +1,65 @@
-import io
-from email.message import EmailMessage
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import io
+import os
+import matplotlib.pyplot as plt
+from PIL import Image
+import random
+import string
 from dotenv import load_dotenv
-import os 
+
+
+def random_filename():
+    # Gerar um nome de arquivo aleatório
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+
+
+def save_images_as_one_file(lista_de_imagens):
+    randomname = random_filename()
+    images = [imagem.savefig(fname=randomname, format='png', bbox_inches='tight', pad_inches=0.1, format='png') for imagem in lista_de_imagens]
+
+    # Combine as imagens em um único arquivo
+    img_buffer = io.BytesIO()
+    images[0].save(img_buffer, format='PNG', save_all=True, append_images=images[1:])
+    
+    return img_buffer.getvalue()
+
 
 def envio_resultado(titulo, mensagem, lista_de_imagens, para):
     load_dotenv()
     de = os.getenv('AUTH_EMAIL')
     senha_de = os.getenv('AUTH_PASS')
+
     try:
         server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-        server_ssl.login(de, senha_de)  
-        msg = EmailMessage()
-        msg.set_content(mensagem)
-        msg['Subject'] = titulo
+        server_ssl.login(de, senha_de)
 
-        for i in lista_de_imagens:
-            formato = "png"
-            f = io.BytesIO()
-            i.savefig(f, format=formato)
-            f.seek(0)
-            img_data = f.read()
-            msg.add_attachment(img_data, maintype='image', subtype=formato)
-            server_ssl.sendmail(de, para, msg.as_string())
+        # Construir a mensagem MIME
+        msg = MIMEMultipart()
+        msg['Subject'] = titulo
+        msg['From'] = de
+        msg['To'] = para
+
+        # Adicionar corpo do e-mail (texto/html)
+        msg.attach(MIMEText(mensagem, 'html'))
+
+        # Criar um único anexo para todas as imagens
+        anexo = MIMEBase('application', 'octet-stream')
+        anexo.set_payload(save_images_as_one_file(lista_de_imagens))
+
+        # Adicionar o anexo ao corpo do e-mail
+        encoders.encode_base64(anexo)
+        
+        msg.attach(anexo)
+
+        # Enviar o e-mail usando smtplib diretamente
+        server_ssl.sendmail(de, para, msg.as_string())
 
     except Exception as e:
-        # Print any error messages to stdout
+        # Imprimir mensagens de erro
         print(e)
     finally:
-        server_ssl.quit() 
+        server_ssl.quit()
